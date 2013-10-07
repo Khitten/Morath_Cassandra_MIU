0c05bcdbb73da2255b8f584725810bd1d8c25a2c
Cassandra <cmorath@fullsail.edu>
2013-10-07 13:04:53 -0400

    Added the initial Bronze App

---
+++ Morath_Cassandra_MIU_P2_Bronze/JavaScript/JSONBronze.js
@@ -0,0 +1,226 @@
+// JavaScript Document
+
+
+
+var json = {
+	"petInfo1"   : {
+		"oname"  :  ["Owner's Name:", "Alex"],
+		"onotes" :  ["Owner's Notes:", "Owner is particular"],
+		"pname"  :  ["Pet's Name:", "Zeus"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-02-02"],
+		"slider" :  ["Difficulty:", "3"],
+		"gnotes" :  ["Grooming Notes:", "#7 all over"],
+		"pnotes" :  ["Pet Notes:", "Good boy!"],
+		"sex"    :  ["Sex:", "Neutered"]
+	},
+	"petInfo2"   : {
+		"oname"  :  ["Owner's Name:", "Bob"],
+		"onotes" :  ["Owner's Notes:", "Owner is nice"],
+		"pname"  :  ["Pet's Name:", "Yaya"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo3"   : {
+		"oname"  :  ["Owner's Name:", "Cass"],
+		"onotes" :  ["Owner's Notes:", "Owner is crazy"],
+		"pname"  :  ["Pet's Name:", "Xena"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo4"   : {
+		"oname"  :  ["Owner's Name:", "Dawn"],
+		"onotes" :  ["Owner's Notes:", "Owner is sweet"],
+		"pname"  :  ["Pet's Name:", "Windy"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo5"   : {
+		"oname"  :  ["Owner's Name:", "Elfie"],
+		"onotes" :  ["Owner's Notes:", "Owner is lofty"],
+		"pname"  :  ["Pet's Name:", "Victor"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo6"   : {
+		"oname"  :  ["Owner's Name:", "Frankie"],
+		"onotes" :  ["Owner's Notes:", "Owner is cheap"],
+		"pname"  :  ["Pet's Name:", "Ubu"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Male"]
+	},
+	"petInfo7"   : {
+		"oname"  :  ["Owner's Name:", "George"],
+		"onotes" :  ["Owner's Notes:", "Owner is wonderful"],
+		"pname"  :  ["Pet's Name:", "Tugger"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Female"]
+	},
+	"petInfo8"   : {
+		"oname"  :  ["Owner's Name:", "Hank"],
+		"onotes" :  ["Owner's Notes:", "Owner is superior"],
+		"pname"  :  ["Pet's Name:", "Sissy"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo9"   : {
+		"oname"  :  ["Owner's Name:", "Issis"],
+		"onotes" :  ["Owner's Notes:", "Owner is insane"],
+		"pname"  :  ["Pet's Name:", "Rock"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Male"]
+	},
+	"petInfo10"   : {
+		"oname"  :  ["Owner's Name:", "John"],
+		"onotes" :  ["Owner's Notes:", "Owner is doting"],
+		"pname"  :  ["Pet's Name:", "Que"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo11"   : {
+		"oname"  :  ["Owner's Name:", "Karen"],
+		"onotes" :  ["Owner's Notes:", "Owner is in a hurry"],
+		"pname"  :  ["Pet's Name:", "Prissy"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Female"]
+	},
+	"petInfo12"   : {
+		"oname"  :  ["Owner's Name:", "Lem"],
+		"onotes" :  ["Owner's Notes:", "Owner is a nut"],
+		"pname"  :  ["Pet's Name:", "Olsen"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo13"   : {
+		"oname"  :  ["Owner's Name:", "May"],
+		"onotes" :  ["Owner's Notes:", "Owner is windsome"],
+		"pname"  :  ["Pet's Name:", "Nana"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo14"   : {
+		"oname"  :  ["Owner's Name:", "North"],
+		"onotes" :  ["Owner's Notes:", "Owner is a friend"],
+		"pname"  :  ["Pet's Name:", "Major"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Neutered"]
+	},
+	"petInfo15"   : {
+		"oname"  :  ["Owner's Name:", "Owen"],
+		"onotes" :  ["Owner's Notes:", "Owner is active"],
+		"pname"  :  ["Pet's Name:", "Lacy"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo16"   : {
+		"oname"  :  ["Owner's Name:", "Pop"],
+		"onotes" :  ["Owner's Notes:", "Owner is slow"],
+		"pname"  :  ["Pet's Name:", "Kipper"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Unknown"]
+	},
+	"petInfo17"   : {
+		"oname"  :  ["Owner's Name:", "Quint"],
+		"onotes" :  ["Owner's Notes:", "Owner is nice"],
+		"pname"  :  ["Pet's Name:", "Juicy"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Spayed"]
+	},
+	"petInfo18"   : {
+		"oname"  :  ["Owner's Name:", "Russ"],
+		"onotes" :  ["Owner's Notes:", "Owner is mean"],
+		"pname"  :  ["Pet's Name:", "Isabel"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Neutered"]
+	},
+	"petInfo19"   : {
+		"oname"  :  ["Owner's Name:", "Steve"],
+		"onotes" :  ["Owner's Notes:", "Owner is hostile"],
+		"pname"  :  ["Pet's Name:", "Henry"],
+		"breed"  :  ["Breed:", "Cats"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Male"]
+	},
+	"petInfo20"   : {
+		"oname"  :  ["Owner's Name:", "Tango"],
+		"onotes" :  ["Owner's Notes:", "Owner is adorable"],
+		"pname"  :  ["Pet's Name:", "Guss"],
+		"breed"  :  ["Breed:", "Dogs"],
+		"gdate"  :  ["Groom Date:", "2013-01-01"],
+		"slider" :  ["Difficulty:", "6"],
+		"gnotes" :  ["Grooming Notes:", "#10 all over"],
+		"pnotes" :  ["Pet Notes:", "Good girl!"],
+		"sex"    :  ["Sex:", "Unknown"]
+	}
+}
